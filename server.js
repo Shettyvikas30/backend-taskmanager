dfc0b1e6850d206b7b37e45dafee42f180fc1ad0
Vikas J Shetty <shettyvikas30@gmail.com>
2025-05-03 19:27:28 +0530

    Initial commit for backend1

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
+// Routes
 app.use('/api/tasks', require('./routes/tasks'));
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+// Use dynamic port for Render
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
